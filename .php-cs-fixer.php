94798b4f728b9a02e6adb0241588b8189400fe1b
boston <bost56@gmail.com>
2022-02-01 19:17:58 +0300

    update php-cs-fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -13,12 +13,12 @@ $config = new PhpCsFixer\Config();
 return $config
     ->setRiskyAllowed(true)
     ->setRules(array(
-        '@PSR2'                => true,
-        'strict_param'         => true,
-        'declare_strict_types' => true,
-        'array_syntax'         => ['syntax' => 'short'],
-        'single_quote'         => true,
-        'native_function_invocation'=>true,
+        '@PSR2'                      => true,
+        'strict_param'               => true,
+        'declare_strict_types'       => true,
+        'array_syntax'               => ['syntax' => 'short'],
+        'single_quote'               => true,
+        'native_function_invocation' => ['include'=>['@all']],
     ))
     ->setFinder($finder)
     ->setUsingCache(true);
